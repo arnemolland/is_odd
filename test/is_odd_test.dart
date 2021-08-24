8d9bf9cf054b2b6f25e36f914aa2a5ede39b7890
jwd25 <jwinterday@mail.ru>
2021-08-24 14:01:09 +0300

    check boundary

--- test/is_odd_test.dart
+++ test/is_odd_test.dart
@@ -18,5 +18,20 @@ void main() {
     test('isOdd() returns false for even number', () {
       expect(isOdd(evenNumber), isFalse);
     });
+
+    test('isOdd() correct int boundary', () {
+      const maxValue = 2 ^ 53 + 1;
+      final values = List<int>.generate(maxValue, (int index) => index);
+
+      for (var val in values) {
+        final isOdd = val % 2 != 0;
+
+        if (isOdd) {
+          expect(isOdd, isTrue);
+        } else {
+          expect(isOdd, isFalse);
+        }
+      }
+    });
   });
 }
